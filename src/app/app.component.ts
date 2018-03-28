09128b33e3fcd90d0ca757b3005f96e1e2905c22
David Lewis and Ron Craig <david@>
2018-03-28 09:38:45 -0700

    add two kombucha brands using ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,8 @@ export class AppComponent {
   title = 'Kombucha Bar';
 
   kombuchas: Kombucha[] = [
-    // new Kombucha
-
+    new Kombucha('Liquid Air', 'High As a Sky', 4.50, 30),
+    new Kombucha('2 Day Tea Brew', 'NoPo', 2, 10)
   ]
-
-
 
 }
