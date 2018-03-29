f1c9b9ab0a885d5243d1f2f1c352ac78311977c5
David Lewis and Ronald Craig <ronald@>
2018-03-29 15:31:08 -0700

    add more happy hour button bugs, might revert before happy hour button commit

--- src/app/happyHour.pipe.ts
+++ src/app/happyHour.pipe.ts
@@ -8,13 +8,12 @@ import { Kombucha } from './models/kombucha.model';
 
 export class HappyHourPipe implements PipeTransform {
   transform(input: Kombucha[], happyHour) {
-    console.log(happyHour);
     for (let i = 0; i < input.length; i++) {
       if(happyHour && (input[i].price >= 1)) {
-        console.log("here");
-        input[i].price--;
+        console.log(input[i]);
+        let newPrice = input[i].price--;
       }
-    }
+}
     return input;
   }
 }
